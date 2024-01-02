9b684661ea7e7235ac900d6c52f3b32dc38a2a57
ACER <ACER@DESKTOP-E3FS00P>
2024-01-02 21:42:37 +0500

    Add project files.

---
+++ Controller/HomeController.cs
@@ -0,0 +1,48 @@
+﻿
+using E_commercebyifza.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ECommerceController : ControllerBase
+{
+    private readonly ECommerceDbContext _dbContext;
+
+    public ECommerceController(ECommerceDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+    {
+        var products = await _dbContext.Products.ToListAsync();
+        return Ok(products);
+    }
+
+    
+    [HttpPost]
+    public async Task<ActionResult<Product>> PostProduct(Product product)
+    {
+        _dbContext.Products.Add(product);
+        await _dbContext.SaveChangesAsync();
+        return CreatedAtAction(nameof(GetProducts), new { id = product.ProductId }, product);
+    }
+
+    
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutProduct(int id, Product product)
+    {
+        if (id != product.ProductId)
+        {
+            return BadRequest();
+        }
+
+        _dbContext.Entry(product).State = EntityState.Modified;
+        await _dbContext.SaveChangesAsync();
+
+        return NoContent();
+    }
+}
---
+++ Data/ECommerceDbContext.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Reflection.Emit;
+using Microsoft.EntityFrameworkCore;
+namespace E_commercebyifza.Data
+
+
+    public class ECommerceDbContext : DbContext
+    {
+       
+    public ECommerceDbContext(DbContextOptions<ECommerceDbContext> options)
+        : base(options)
+        { 
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+        }
+    }
+
+}
+
